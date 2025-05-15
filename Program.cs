532bedc6222a6298f97a15c19c12917d4c070f15
robertobr122 <rbbragaengenharia@gmail.com>
2025-05-15 10:29:52 -0300

    fix: Enviando a primeira versão da funcionalidade sair

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ class ProjetoCrudEventos
 
     static void ListarOpcoes()
     {
-        System.Console.WriteLine("Selecione a opção que deseja: \n\n-------------------------\n");
+        System.Console.WriteLine("Selecione a opção que deseja: \n-------------------------");
         System.Console.WriteLine("1) Cadastrar Evento \n2) Listar Eventos \n3) Atualizar Evento \n4) Deletar Evento \n5) Sair");
         string entrada = Console.ReadLine();
 
@@ -26,6 +26,7 @@ class ProjetoCrudEventos
             Console.Clear();
             ListarOpcoes();
             return;
+
         }
 
         switch (selecao)
@@ -75,6 +76,50 @@ class ProjetoCrudEventos
     }
     static void Sair()
     {
+        Console.Clear();
+        System.Console.WriteLine("\nCarregando...");
+        Thread.Sleep(3000);
+        Console.Clear();
+        System.Console.WriteLine("Tem certeza que deseja finalizar o Sistema?\n");
+        System.Console.WriteLine("Selecione uma opção: \n1) SIM \n2) NÃO\n");
+        System.Console.WriteLine("ATENÇÃO: Caso selecione a opção [ 1 ], o sistema será finalizado e não ficará nada salvo por falta de integração com o bando de dados\n");
 
+        string confirmarSaida = Console.ReadLine();
+
+        if (!int.TryParse(confirmarSaida, out int confirmacao))
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nSelecione a opção [ 1 ] ou [ 2 ]");
+            Thread.Sleep(3000);
+            Sair();
+            return;
+        }
+
+        if (confirmacao > 2 || confirmacao < 1)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nSelecione a opção [ 1 ] ou [ 2 ]");
+            Thread.Sleep(3000);
+            Sair();
+            return;
+
+        }
+        else if (confirmacao == 1)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nEncerrando Sistema...\n");
+            Thread.Sleep(3000);
+            return;
+
+        }
+        else
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nVoltando para o menu inicial...");
+            Thread.Sleep(3000);
+            Console.Clear();
+            ListarOpcoes();
+            return;
+        }
     }
 }
